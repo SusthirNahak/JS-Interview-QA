756db350b8b42a15dc478243bd09a2eb4e0e70f1
susthir <susthir19march@gmail.com>
2024-04-10 12:20:29 +0530

    closure

---
+++ JSInterviewQuestions/topic_4.js
@@ -0,0 +1,30 @@
+//closure in javascript
+//closure in javascript- it gives access to outer functions scope from inner function
+
+//lexical scope
+/*
+lexical scope :-a variable defined outside the function ,
+can be accessable inside of another function defined after a variable declaration but opposite is not true,
+the variable defined inside the function can not accessable outside the function.
+*/
+//example
+// var uname = "kanha";
+// //global scope
+// function local(){
+//     //local scope
+//     console.log(uname);
+// }
+// local();
+
+//example
+// global scope
+function subscribe(){
+    var uname = "kanha";
+    //inner scope 2
+    function DisplayName(){ // DisplayName is called closure
+        //inner scope 1
+        // alert(uname)
+    }
+    DisplayName()
+}
+subscribe();
